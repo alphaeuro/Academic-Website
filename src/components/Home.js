7dd06ed0e40f03d09db3e7787a8e9e8def35771c
Your Name <abhaynimoria2612@gmail.com>
2024-02-07 15:33:51 +0530

    navbar created

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,27 @@ import { Container, Typography } from '@mui/material';
 const Home = () => {
   return (
     <Container>
-      <Typography variant="h4" gutterBottom>
-        Welcome to My Academic Website
+      <Typography variant="h3" gutterBottom  style={{ marginTop: '10px' ,  fontFamily: 'Montserrat, sans-serif' , textDecoration: 'underline' , fontWeight: 'bold'}} >
+        The Ghosal Laboratory
       </Typography>
-      <Typography variant="body1">
-        I am Professor [Your Name], and this is my personal academic website. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+      <Typography variant="h5" gutterBottom  style={{ marginTop: '10px' ,  fontFamily: 'Montserrat, sans-serif', textDecoration: 'underline' , fontWeight: 'semibold'}}>
+       Rural geography research group
       </Typography>
+      <Typography variant="h5" gutterBottom  style={{ marginTop: '10px' ,  fontFamily: 'Montserrat, sans-serif' , textDecoration: 'underline' , fontWeight: 'semibold'}}>
+       policy, planning and development
+      </Typography>
+      <Typography variant="h7" gutterBottom  style={{ marginTop: '10px' ,  fontFamily: 'Montserrat, sans-serif' , fontSize: '15px' , fontWeight: 'bold'}}>
+       Research Areas:<br />
+      </Typography>
+      <Typography variant="body" gutterBottom style={{ marginTop: '10px' ,  fontFamily: 'Montserrat, sans-serif' , fontSize: '13px' , }}>
+      •Rural Development and Regional Planning <br />
+      •Ethnographic and Qualitative Research <br />
+      •Social Forestry for Entrepreneurship <br />
+      •Rural Water, Health, & Education <br />
+      •Public Policy for Science & Society
+      </Typography>
+
+
     </Container>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,20 @@
 // Navbar.js
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">Your Name</Typography>
+    <AppBar position="static" sx={{ backgroundColor: '#008080', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)' , justifyContent: 'space-between' , height: '50px' }}>
+      <Toolbar sx={{ padding: '0 1rem', display: 'flex', alignItems: 'right', justifyContent: 'flex-end' }}>
+      <Typography variant="h6" component="div" sx={{ display: 'flex', top: '-100px' }} style={{ fontFamily: 'Montserrat, sans-serif' }}>
+        </Typography>
+        <Button color="inherit" sx={{ marginRight: '1rem', '&:hover': { backgroundColor: '#20b2aa' } }} >Home</Button>
+        <Button color="inherit" sx={{ marginRight: '1rem', '&:hover': { backgroundColor: '#20b2aa' } }}>Dr. Somnath Ghosal</Button>
+        <Button color="inherit" sx={{ marginRight: '1rem', '&:hover': { backgroundColor: '#20b2aa' } }}>Projects</Button>
+        <Button color="inherit" sx={{ marginRight: '1rem', '&:hover': { backgroundColor: '#20b2aa' } }}>Publications</Button>
+        <Button color="inherit" sx={{ marginRight: '1rem', '&:hover': { backgroundColor: '#20b2aa' } }}>News</Button>            
       </Toolbar>
     </AppBar>
   );
