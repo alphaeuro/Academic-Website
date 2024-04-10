cf6118997f104eae165f9432b345585cb24a2682
alphaeuro <anshujha200230@gmail.com>
2024-04-10 07:51:51 +0530

    publi comit test

--- src/components/extra.js
+++ src/components/extra.js
@@ -1,111 +1,41 @@
 import React from 'react';
-import './About.css';
-import { FaPhone, FaEnvelope, FaYoutube, FaTwitter, FaFacebook } from 'react-icons/fa'; // Import icons from Font Awesome library
+import './Publications.css'; // Import CSS file for styling
 
-const About = () => {
+const Publications = () => {
   return (
-    <div className="about-container">
+    <div className="pub-container">
       {/* Container for the image and text side by side */}
-      <div className="image-and-text-container">
-        <div className="image-container">
-          <img src="https://source.unsplash.com/NXiIVnzBwZ8" alt="Jane Writer" className="about-image" />
+      <div className="image-and-text-pub-container">
+        <div className="image-pub-container">
+          <img src="https://source.unsplash.com/NXiIVnzBwZ8" alt="Jane Writer" className="pub-image" />
         </div>
-        <div className="text-container">
-          <h1>Dr Somnath Ghosal</h1>
-          <h4>Assistant Professor
-            <br />
-            Centre for Rural Development in Innovative & Sustainable Technology, IIT Kharagpur</h4>
-          <div className="contact-icons">
-            <a href="tel:+91(0)3222283894" className="icon-link"><FaPhone /> </a>
-            <a href="mailto:drsomnathghosal@gmail.com" className="icon-link"><FaEnvelope /></a>
-          </div>
+        <div className="text-pub-container">
+          <h3>Author of Sustainable Forestry: People, Culture and Economics<br/>ISBN: 978-81-910382-9-3<br/>L. G. Publishers Distributors, New Delhi, (2013)</h3>
         </div>
       </div>
-      {/* Education section below */}
-      <div className="education-section">
-        <h1>Education</h1>
-        <div className="education-container">
-          <div className="education-item">
-            <img src="https://via.placeholder.com/150" alt="Education 1" />
-            <p>MA in Geography,<br></br> Visva-Bharati University, India <br></br> 2005</p>
-          </div>
-          <div className="education-item">
-            <img src="https://via.placeholder.com/150" alt="Education 2" />
-            <p>MSc in Human Geography, School of Geography, University of Nottingham, UK <br></br> 2006</p>
-          </div>
-        </div>
-      </div>
-      {/* Experience section below */}
-      <div className="experience-section">
-        <h1>Experience</h1>
-        <div className="timeline">
-          <div className="timeline-item">
-            <div className="timeline-date">2008 - 2010</div>
-            <div className="timeline-content">
-              <p>Graduate level teaching assistant and demonstrator, School of Geography, University of Nottingham, UK</p>
-            </div>
-          </div>
-          <div className="timeline-item">
-          <div className="timeline-date">2010 - 2013</div>
-            <div className="timeline-content">
-              <p>Assistant Professor in Geography, University of Nottingham, UK</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* Responsibilities section below */}
-      <div className="responsibilities-section">
-        <h3>During his ventures, Dr. Somnath Ghosal also received various positions of responsibility . . .</h3>
-        <div className="responsibilities-container">
-          <div className="responsibility-item">
-            <div className="responsibility-image-container">
-              <img src="https://via.placeholder.com/150" alt="Responsibility 1" />
-            </div>
-            <div className="responsibility-details">
-              <h4>September 2023 - December 2023</h4>
-              <p>Resource Person, upskilling Leadership Qualities of Sabhapaties for Rural Transformation and Modern Agricultural Practices, Department of Panchayats and Rural Development, Govt. of West Bengal.</p>
-            </div>
-          </div>
-          <div className="responsibility-item">
-            <div className="responsibility-image-container">
-              <img src="https://via.placeholder.com/150" alt="Responsibility 2" />
-            </div>
-            <div className="responsibility-details">
-              <h4>2017-2019</h4>
-              <p>Member, National Level Monitoring Programme, Ministry of Rural Development, Government of India.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*Awards and Honours section below */}
-      <div className='awards-section'>
-        <h1>Awards and Honours</h1>
-        <div className="awards-container">
-          <div className="award-item">
-            <img src="https://via.placeholder.com/150" alt="Award 1" />
-            <p>Award 1 description goes here.</p>
-          </div>
-          <div className="award-item">
-            <img src="https://via.placeholder.com/150" alt="Award 2" />
-            <p>Award 2 description goes here.</p>
-          </div>
-          <div className="award-item">
-            <img src="https://via.placeholder.com/150" alt="Award 3" />
-            <p>Award 3 description goes here.</p>
-          </div>
-          <div className="award-item">
-            <img src="https://via.placeholder.com/150" alt="Award 4" />
-            <p>Award 4 description goes here.</p>
-          </div>
-          <div className="award-item">
-            <img src="https://via.placeholder.com/150" alt="Award 5" />
-            <p>Award 5 description goes here.</p>
-          </div>
+      <div className="publi-section">
+        <br/>
+        <h2>Rural Livelihoods Management and Public Policy:</h2>
+        <div className="publi-content">
+          <h3>1. Goswami, T. and Ghosal, S. (2022)</h3>
+          <p>From Rice Fields to Brackish Water Farms: Changing Livelihoods in Agrarian Coastal Bengal, India. <br/>Asia-Pacific Journal of Regional Science. Vol. 6 (2), p. 453 - 484. <br/>DOI: 10.1007/s41685-022-00229-8.</p>
+          <h3>2. Ghosh M, Ghosal S. (2021)</h3>
+          <p>Multidimensional Rural Livelihoods in Indian Sub-Himalaya: Regional Analysis, Households Well-Being and Its Determinants. <br/>International Journal of Rural Management, Vol. 19, p. 45 - 63.<br/> DOI:10.1177/09730052211047639.</p>
+          <h3>3. Ghosh, M. and Ghosal, S. (2021)</h3>
+          <p>Household's Choices and Their Drivers to Rural Non-Farm Livelihood Diversification in West Bengal, India. <br/>Journal of Asian and African Studies, Vol. 57, p. 1158 - 1178. <br/>DOI: 10.1177/00219096211040592.</p>
+          <h3>4. Ghosh, M. and Ghosal, S. (2019)</h3>
+          <p>Living With Insecurity: A Critical Examination of the Environmental Problems Linked with Social Problems in Rural West Bengal, India. <br/>Romanian Journal of Geography, Vol. 63 (1), p. 53 - 64.</p>
+          <h3>5. Ghosal, S. (2013)</h3>
+          <p>Soft or Hard: Infrastructure Matters Rural Economic Empowerment. <br/>Journal of Infrastructure Development (Sage, ISSN: 0974-9306, eISSN: 0975-5969), Vol. 5 (2), p. 137 - 149.</p>
+          <h3>6. Ghosal, S. (2012)</h3>
+          <p>Integral Economics and the Exploration of Indigenous Knowledge. <br/>Corvinus Journal of Sociology and Social Policy (Corvinus University of Budapest, ISSN: 2062-087X), Vol. 3 (2), p. 83 – 105.</p>
+          <h3>Item 5: Title</h3>
+          <p>Description of Item 5.</p>
+          
         </div>
       </div>
     </div>
   );
 };
 
-export default About;
-
+export default Publications;
--- src/components/publications.js
+++ src/components/publications.js
@@ -1,40 +1,21 @@
-import React from 'react';
-import './Publications.css'; // Import CSS file for styling
+
+import React from "react";
+import "./Home.css";
 
 const Publications = () => {
+  
+
   return (
-    <div className="pub-container">
-      {/* Container for the image and text side by side */}
-      <div className="image-and-text-pub-container">
-        <div className="image-pub-container">
-          <img src="https://source.unsplash.com/NXiIVnzBwZ8" alt="Jane Writer" className="pub-image" />
+    <div className="home-container">
+      
+          <h1>Dr. Somnath Ghosal</h1>
+          <h2>Publications</h2>
+          <p>
+            Brief introduction goes here. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </p>
         </div>
-        <div className="text-pub-container">
-          <h3>Author of Sustainable Forestry: People, Culture and Economics<br/>ISBN: 978-81-910382-9-3<br/>L. G. Publishers Distributors, New Delhi, (2013)</h3>
-        </div>
-      </div>
-      <div className="publi-section">
-        <br/>
-        <h2>Rural Livelihoods Management and Public Policy:</h2>
-        <div className="publi-content">
-          <h3>1. Goswami, T. and Ghosal, S. (2022)</h3>
-          <p>From Rice Fields to Brackish Water Farms: Changing Livelihoods in Agrarian Coastal Bengal, India. <br/>Asia-Pacific Journal of Regional Science. Vol. 6 (2), p. 453 - 484. <br/>DOI: 10.1007/s41685-022-00229-8.</p>
-          <h3>2. Ghosh M, Ghosal S. (2021)</h3>
-          <p>Multidimensional Rural Livelihoods in Indian Sub-Himalaya: Regional Analysis, Households Well-Being and Its Determinants. <br/>International Journal of Rural Management, Vol. 19, p. 45 - 63.<br/> DOI:10.1177/09730052211047639.</p>
-          <h3>3. Ghosh, M. and Ghosal, S. (2021)</h3>
-          <p>Household's Choices and Their Drivers to Rural Non-Farm Livelihood Diversification in West Bengal, India. <br/>Journal of Asian and African Studies, Vol. 57, p. 1158 - 1178. <br/>DOI: 10.1177/00219096211040592.</p>
-          <h3>4. Ghosh, M. and Ghosal, S. (2019)</h3>
-          <p>Living With Insecurity: A Critical Examination of the Environmental Problems Linked with Social Problems in Rural West Bengal, India. <br/>Romanian Journal of Geography, Vol. 63 (1), p. 53 - 64.</p>
-          <h3>5. Ghosal, S. (2013)</h3>
-          <p>Soft or Hard: Infrastructure Matters Rural Economic Empowerment. <br/>Journal of Infrastructure Development (Sage, ISSN: 0974-9306, eISSN: 0975-5969), Vol. 5 (2), p. 137 - 149.</p>
-          <h3>6. Ghosal, S. (2012)</h3>
-          <p>Integral Economics and the Exploration of Indigenous Knowledge. <br/>Corvinus Journal of Sociology and Social Policy (Corvinus University of Budapest, ISSN: 2062-087X), Vol. 3 (2), p. 83 – 105.</p>
-          <h3>Item 5: Title</h3>
-          <p>Description of Item 5.</p>
-          
-        </div>
-      </div>
-    </div>
+      
   );
 };
 
