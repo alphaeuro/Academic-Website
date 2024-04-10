ef407f5b6d0899d6d67bca0f428e0ef8483c480c
alphaeuro <anshujha200230@gmail.com>
2024-04-10 05:45:59 +0530

    ABOUT PAGE COMPLETED DONE

--- src/components/About.js
+++ src/components/About.js
@@ -11,10 +11,8 @@ const About = () => {
           <img src="https://source.unsplash.com/NXiIVnzBwZ8" alt="Jane Writer" className="about-image" />
         </div>
         <div className="text-container">
-          <h1>Dr Somnath Ghoshal</h1>
-          <h4>Assistant Professor
-            <br />
-            Centre for Rural Development in Innovative & Sustainable Technology, IIT Kharagpur</h4>
+          <h1>Dr Somnath Ghosal</h1>
+          <h4>Assistant Professor,<br/>Centre for Rural Development & Innovative Sustainable Technology,<br/>IIT Kharagpur</h4>
           <div className="contact-icons">
             <a href="tel:+91(0)3222283894" className="icon-link"><FaPhone /> </a>
             <a href="mailto:drsomnathghosal@gmail.com" className="icon-link"><FaEnvelope /></a>
@@ -30,18 +28,18 @@ const About = () => {
         <div className="education-container">
           <div className="education-item">
             <img src="https://via.placeholder.com/150" alt="Education 1" />
-            <p>MA in Geography,<br></br> Visva-Bharati University, India</p>
-            <p>2005</p>
+            <p>MA in Geography,<br></br> Visva-Bharati University, India <br></br> 2005</p>
+            {/* <p>2005</p> */}
           </div>
           <div className="education-item">
             <img src="https://via.placeholder.com/150" alt="Education 2" />
-            <p>MSc in Human Geography, School of Geography, University of Nottingham, UK</p>
-            <p>2006</p>
+            <p>MSc in Human Geography, School of Geography, University of Nottingham, UK <br></br> 2006</p>
+            {/* <p>2006</p> */}
           </div>
           <div className="education-item">
             <img src="https://via.placeholder.com/150" alt="Education 3" />
-            <p>PhD in Environment & Society, School of Geography, University of Nottingham, UK</p>
-            <p>2010</p>
+            <p>PhD in Environment & Society, School of Geography, University of Nottingham, UK<br></br>2010</p>
+            {/* <p>2010</p> */}
           </div>
         </div>
       </div>
@@ -122,6 +120,62 @@ const About = () => {
               <h4>April 2010 - June 2010</h4>
               <p>Research Assistant for GIS4EU project, Centre for Geospatial Science (CGS), University of Nottingham, UK.</p>
             </div>
+          </div>
+        </div>
+      </div>
+      {/*Awards and Honours section below */}
+      <div className='awards-section'>
+        <h1>Awards & Honours</h1>
+        <div className="awards-container">
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 1" />
+            <p>Outstation Chairman for Rural Technology, Social Organizations, and Rural Development Thematic Panel of the 47th Indian Social Science Congress<br></br>2023</p>
+          </div>
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 2" />
+            <p>Outstation Chairman for Rural Technology, Social Organizations, and Rural Development Thematic Panel of the 46th Indian Social Science Congress <br></br>2022</p>
+          </div>
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 3" />
+            <p>Patent Filed on Low-Cost Drinking Water Filtration System <br></br>2021</p>
+          </div>
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 4" />
+            <p>Copy Right Certificate for Designing Purified Drinking Water Distribution at Nominal Cost<br></br>2018</p>
+          </div>
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 5" />
+            <p>Glory of India Award <br></br>2013</p>
+          </div>
+        </div>
+      </div>
+      {/* Courses Offered section below */}
+      <div className="courses-section">
+        <h1>UG & PG Courses Offered</h1>
+        <div className="courses-container">
+          <div className="course-item">
+            <p>1. Fundamentals of Rural Innovation (RD30005)</p>
+          </div>
+          <div className="course-item">
+            <p>2. Participatory Rural Appraisal (RD61001)</p>
+          </div>
+          <div className="course-item">
+            <p>3. Natural Resource Management: Science, Technology, and Policy</p>
+          </div>
+          <div className="course-item">
+            <p>4. Rural Development: Concept, Strategies, and Experiences</p>
+          </div>
+          <div className="course-item">
+            <p>5. Transfer and Management of Rural Technology (RD30002)</p>
+          </div>
+          <div className="course-item">
+            <p>6. Financing Infrastructure Projects (ID60003)</p>
+          </div>
+          <div className="course-item">
+            <p>7. Sustainable Green Urban Planning</p>
+          </div>
+          <div className="course-item">
+            <p>8. Social Research Methodologies (Qualitative): Questionnaire Survey, Interviews, Focus Group</p>
           </div>
         </div>
       </div>
--- src/components/extra.js
+++ src/components/extra.js
@@ -11,87 +11,95 @@ const About = () => {
           <img src="https://source.unsplash.com/NXiIVnzBwZ8" alt="Jane Writer" className="about-image" />
         </div>
         <div className="text-container">
-          <h1>Dr Somnath Ghoshal</h1>
+          <h1>Dr Somnath Ghosal</h1>
           <h4>Assistant Professor
             <br />
             Centre for Rural Development in Innovative & Sustainable Technology, IIT Kharagpur</h4>
           <div className="contact-icons">
             <a href="tel:+91(0)3222283894" className="icon-link"><FaPhone /> </a>
             <a href="mailto:drsomnathghosal@gmail.com" className="icon-link"><FaEnvelope /></a>
-            <a href="https://www.youtube.com/@somnathghosal8947" className="icon-link"><FaYoutube /></a>
-            <a href="https://twitter.com/SomnathGhosal13" className="icon-link"><FaTwitter /></a>
-            <a href="https://www.facebook.com/drsomnathghosal" className="icon-link"><FaFacebook /></a>
           </div>
         </div>
       </div>
       {/* Education section below */}
       <div className="education-section">
-        <h2>Education</h2>
-        <div className="education-container"><div className="education-item">
+        <h1>Education</h1>
+        <div className="education-container">
+          <div className="education-item">
             <img src="https://via.placeholder.com/150" alt="Education 1" />
-            <p>MA in Geography,<br></br> Visva-Bharati University, India</p>
-            <p>2005</p>
+            <p>MA in Geography,<br></br> Visva-Bharati University, India <br></br> 2005</p>
           </div>
           <div className="education-item">
             <img src="https://via.placeholder.com/150" alt="Education 2" />
-            <p>MSc in Human Geography, School of Geography, University of Nottingham, UK</p>
-            <p>2006</p>
-          </div>
-          <div className="education-item">
-            <img src="https://via.placeholder.com/150" alt="Education 3" />
-            <p>PhD in Environment & Society, School of Geography, University of Nottingham, UK</p>
-            <p>2010</p>
+            <p>MSc in Human Geography, School of Geography, University of Nottingham, UK <br></br> 2006</p>
           </div>
         </div>
       </div>
       {/* Experience section below */}
       <div className="experience-section">
-        <h2>Experience</h2>
+        <h1>Experience</h1>
         <div className="timeline">
           <div className="timeline-item">
+            <div className="timeline-date">2008 - 2010</div>
             <div className="timeline-content">
-              <h3>Position 1</h3>
-              <p>Description of position 1</p>
+              <p>Graduate level teaching assistant and demonstrator, School of Geography, University of Nottingham, UK</p>
             </div>
-            <div className="timeline-date">2008 - 2012</div>
           </div>
           <div className="timeline-item">
+          <div className="timeline-date">2010 - 2013</div>
             <div className="timeline-content">
-              <h3>Position 2</h3>
-              <p>Description of position 2</p>
+              <p>Assistant Professor in Geography, University of Nottingham, UK</p>
             </div>
-            <div className="timeline-date">2013 - 2016</div>
           </div>
-          {/* Add more timeline items as needed */}
         </div>
       </div>
       {/* Responsibilities section below */}
       <div className="responsibilities-section">
-        <h2>Responsibilities</h2>
+        <h3>During his ventures, Dr. Somnath Ghosal also received various positions of responsibility . . .</h3>
         <div className="responsibilities-container">
           <div className="responsibility-item">
-            <img src="https://via.placeholder.com/150" alt="Responsibility 1" />
-            <div className="responsibility-details">
-              <h3>Responsibility 1</h3>
-              <p>Description of responsibility 1</p>
+            <div className="responsibility-image-container">
+              <img src="https://via.placeholder.com/150" alt="Responsibility 1" />
             </div>
-            <div className="responsibility-date">Date 1</div>
+            <div className="responsibility-details">
+              <h4>September 2023 - December 2023</h4>
+              <p>Resource Person, upskilling Leadership Qualities of Sabhapaties for Rural Transformation and Modern Agricultural Practices, Department of Panchayats and Rural Development, Govt. of West Bengal.</p>
+            </div>
           </div>
           <div className="responsibility-item">
-            <img src="https://via.placeholder.com/150" alt="Responsibility 2" />
-            <div className="responsibility-details">
-              <h3>Responsibility 2</h3>
-              <p>Description of responsibility 2</p>
+            <div className="responsibility-image-container">
+              <img src="https://via.placeholder.com/150" alt="Responsibility 2" />
             </div>
-            <div className="responsibility-date">Date 2</div>
+            <div className="responsibility-details">
+              <h4>2017-2019</h4>
+              <p>Member, National Level Monitoring Programme, Ministry of Rural Development, Government of India.</p>
+            </div>
           </div>
-          <div className="responsibility-item">
-            <img src="https://via.placeholder.com/150" alt="Responsibility 3" />
-            <div className="responsibility-details">
-              <h3>Responsibility 3</h3>
-              <p>Description of responsibility 3</p>
-            </div>
-            <div className="responsibility-date">Date 3</div>
+        </div>
+      </div>
+      {/*Awards and Honours section below */}
+      <div className='awards-section'>
+        <h1>Awards and Honours</h1>
+        <div className="awards-container">
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 1" />
+            <p>Award 1 description goes here.</p>
+          </div>
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 2" />
+            <p>Award 2 description goes here.</p>
+          </div>
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 3" />
+            <p>Award 3 description goes here.</p>
+          </div>
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 4" />
+            <p>Award 4 description goes here.</p>
+          </div>
+          <div className="award-item">
+            <img src="https://via.placeholder.com/150" alt="Award 5" />
+            <p>Award 5 description goes here.</p>
           </div>
         </div>
       </div>
@@ -100,3 +108,4 @@ const About = () => {
 };
 
 export default About;
+
