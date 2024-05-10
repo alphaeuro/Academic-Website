bc95c827acd214826396d427a07af668062b58c5
abhay9399 <109021441+abhay9399@users.noreply.github.com>
2024-05-11 02:56:55 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,21 +1,30 @@
-
 import React from "react";
 import "./Home.css";
 
 const News = () => {
-  
+  // Array of newspaper cutting image URLs
+  const newspaperCuttings = [
+    News1,
+    News2,
+    News3,
+    News4,
+    News5,
+    News6,
+    News7,
+  ];
 
   return (
     <div className="home-container">
-      
-          <h1>Dr. Somnath Ghosal</h1>
-          <h2>News</h2>
-          <p>
-            Brief introduction goes here. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.
-          </p>
-        </div>
-      
+      <h1>Dr. Somnath Ghosal</h1>
+      <h2>News</h2>
+      <p>
+        Brief introduction goes here. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit.
+      </p>
+      {newspaperCuttings.map((cutting, index) => (
+        <img key={index} src={cutting} alt={`News ${index + 1}`} />
+      ))}
+    </div>
   );
 };
 
