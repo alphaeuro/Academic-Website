08ccb126fdbb9d0ab8bab41ffafbb8f1050aa8ea
Your Name <abhaynimoria2612@gmail.com>
2024-05-12 00:08:17 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import News from './components/News'; 
+import News from './components/news'; 
 import Research from './components/research'; 
 import Publications from './components/Publications'; 
 import About from './components/About';
