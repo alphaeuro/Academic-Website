c1a364836605df47643489eb9fc750f6e03f1dd7
abhay9399 <109021441+abhay9399@users.noreply.github.com>
2024-05-11 19:00:22 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,12 @@ import profileImage from '../images/profilepic.jpeg'
 import YouTubeSection from './youtubesection';
 import ContactSection from './contact';
 // Importing carousel images
-import c1 from '../images/c1.JPG';
 import c2 from '../images/c2.JPG';
 import c3 from '../images/c3.jpg';
 import c4 from '../images/c4.jpg';
 import c5 from '../images/c5.jpg';
 
-const carouselImages = [c1, c2, c3, c4, c5];
+const carouselImages = [c2, c3, c4, c5];
 
 const Home = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -33,9 +32,16 @@ const Home = () => {
           <img src={logoImage} alt="Logo" className="home-image" />
         </div>
         <div className="text-container1" >
-          <h1>Rural Geography Research Group - public Policy, Planning, and Development</h1>
-          
-          <p>The research group primarily focuses on Community participation in research, Science and public policy integration, Sustainable regional planning, Skill development for livelihoods, Rural infrastructure improvement, Social forestry and entrepreneurship</p>
+          <h1>Rural Geography Research Group - Public Policy, Planning, and Development</h1>
+          <br></br>
+          <h3>The research group primarily focuses on - </h3>
+          <ul>
+          <li>Community Participation Action Research</li>
+          <li>Science and public policy integration</li>
+          <li>Skill development for livelihood generation</li>
+          <li>Rural Infrastructure Planning and Development</li>
+          <li>Social forestry and Natural Resource based Entrepreneurship</li>
+          </ul>
         </div>
       </div>
 
@@ -54,14 +60,14 @@ const Home = () => {
         {/* Assuming you have an appropriate image, replace 'hero_image_url' */}
         <div className="text-container2">
           <h1>Meet Dr. Somnath Ghosal</h1>
-        <p>Dr. Somnath Ghosal is an Assistant Professor at the Centre for Rural  Development and Innovative Sustainable Technology (CRDIST) at the Indian  Institute of Technology Kharagpur. With a robust background in  Environment and Society (Human Geography), Dr. Ghosal's research  primarily focuses on bridging the gap between science and societal needs  through innovative, sustainable development strategies. His work  encompasses a wide range of areas, including community participation in  research, integrating science with public policy, and spearheading  initiatives in sustainable regional planning. Dr. Ghosal is deeply  committed to improving rural infrastructure, promoting skill development  for livelihood generation, and advocating for social forestry and green  urban planning. His dedication to environmental conservation and  development project design highlights his commitment to using academic  research for societal benefit. Learn More...</p>
+        <p>Dr. Somnath Ghosal is an Assistant Professor at the Centre for Rural Development and Innovative Sustainable Technology (CRDIST) at the Indian Institute of Technology Kharagpur. With a robust background in Environment and Society (Human Geography), Dr. Ghosal's research primarily focuses on bridging the gap between applied science, technology, and societal needs through innovative, sustainable development strategies. His work encompasses a wide range of areas, including community overt participation in action research, integrating science with public policy, and spearheading initiatives in sustainable regional planning. Dr. Ghosal is deeply committed to improving rural infrastructure, promoting skill development for livelihood generation, and advocating for social forestry and green urban planning. His dedication to environmental conservation and development project design highlights his commitment to using academic research for societal benefit.<br></br><a href="/About" >Learn More...</a></p>
         </div>
         <div className="image-container2">
           <img src={profileImage} alt="Logo" className="home-image" />
         </div>
         
       </div>
-      <h3>See more about our Projects...</h3>
+      <h2>Know more about our Action Research Projects</h2>
       <YouTubeSection />
       <ContactSection />
       
